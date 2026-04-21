5e6e3e20ef2aa571e6f72734b4b6242d53b6a230
Maxiuts <maximili1008@outlook.com>
2026-04-20 18:42:15 -0600

    otra

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-#!/bin/bash#!/bin/bash
-ssh maxim@100.88.140.50 "wsl -d Ubuntu bash -c 'cd /mnt/c/Users/maxim/proyectos/Proyecto_Fi && git pull && docker compose restart app'"
+#!/bin/bash
+ssh maxim@100.88.140.50 "wsl -d Ubuntu bash -c \"cd /mnt/c/Users/maxim/proyectos/Proyecto_Fi && git pull && docker compose restart app\""
